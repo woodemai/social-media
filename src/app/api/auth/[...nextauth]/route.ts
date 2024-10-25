383316ab91a0d684ae4d3cea705b33388e2f2824
Nikolay Savchenko <nikolay.savchenko@redcollar.ru>
2024-10-25 09:52:22 +0300

    update get post

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -2,5 +2,6 @@ import { GET, POST } from '@/auth';
 export {
   /* @next-codemod-ignore `GET` export is re-exported. Check if this component uses `params` or `searchParams`*/
   GET /* @next-codemod-ignore `POST` export is re-exported. Check if this component uses `params` or `searchParams`*/,
+  /* @next-codemod-error `POST` export is re-exported. Check if this component uses `params` or `searchParams`*/
   POST,
 };
