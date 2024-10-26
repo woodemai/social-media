42b694e6b599837fad8b68e279076428d7077b82
Nikolay Savchenko <nikolay.savchenko@redcollar.ru>
2024-10-27 02:26:36 +0300

    add caching for some queries

--- src/config/prisma.ts
+++ src/config/prisma.ts
@@ -1,13 +1,16 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from '@prisma/client/edge';
+import { withAccelerate } from '@prisma/extension-accelerate';
 
 import { env } from '@/env';
 
-const globalForPrisma = global as unknown as { prisma: PrismaClient };
-export const db = globalForPrisma.prisma || new PrismaClient();
+const globalForPrisma = global as unknown as {
+  prisma: PrismaClient;
+};
+export const db = new PrismaClient().$extends(withAccelerate());
 
 const IS_NODE_DEVELOPMENT = env.NODE_ENV === 'development';
 const IS_VERCEL_DEVELOPMENT = env.NEXT_PUBLIC_VERCEL_ENV === 'development';
 
 if (IS_NODE_DEVELOPMENT || IS_VERCEL_DEVELOPMENT) {
-  globalForPrisma.prisma = db;
+  globalForPrisma.prisma = db as unknown as PrismaClient;
 }
--- src/entities/user/data.ts
+++ src/entities/user/data.ts
@@ -1,21 +1,22 @@
 'use server';
 import { type User } from '@prisma/client';
 import { type User as AuthUser } from 'next-auth';
+import { cache } from 'react';
 
 import { auth } from '@/auth';
 import { db } from '@/config/prisma';
 
 import { type FullUser } from './types';
 
-export const getCurrentUser = async (): Promise<AuthUser> => {
+export const getCurrentUser = cache(async (): Promise<AuthUser> => {
   const session = await auth();
   if (!session?.user) {
     throw new Error('Not authenticated');
   }
   return session.user;
-};
+});
 
-export const getFullCurrentUser = async (): Promise<User> => {
+export const getFullCurrentUser = cache(async (): Promise<User> => {
   const { id } = await getCurrentUser();
   const user = await db.user.findUnique({
     where: {
@@ -28,21 +29,27 @@ export const getFullCurrentUser = async (): Promise<User> => {
   }
 
   return user;
-};
+});
 
-export const getUserById = async (id: string): Promise<FullUser | null> => {
-  return await db.user.findUnique({
-    where: { id },
-    include: {
-      _count: {
-        select: {
-          subscribed: true,
-          subscribers: true,
+export const getUserById = cache(
+  async (id: string): Promise<FullUser | null> => {
+    return await db.user.findUnique({
+      where: { id },
+      cacheStrategy: {
+        ttl: 60 * 60,
+        swr: 60,
+      },
+      include: {
+        _count: {
+          select: {
+            subscribed: true,
+            subscribers: true,
+          },
         },
       },
-    },
-  });
-};
+    });
+  },
+);
 
 export const getUserByEmail = async (email: string) => {
   try {
